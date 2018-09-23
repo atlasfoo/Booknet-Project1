b3310847d0dbfc2a1c27820e1203972d48fc1821
TheFoo @WIN <jf_mejiar547@live.com>
2018-09-22 21:07:10 -0600

    added comment form, pend api and latest reviews

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 #DATABASE_URL: postgres://iatnvhyndrywzs:cad16ec57142e7c71475d608cc16e668e649222ea4607224ba70f6a35ec37435@ec2-54-225-97-112.compute-1.amazonaws.com:5432/dbsa319dkemsq3
 
 import os
+import requests
+
 
 import util
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -73,12 +75,30 @@ def registered():
     
 @app.route("/comment/<int:book_id>")
 def comment(book_id):
-    return "Not implemented yet"
+    book = db.execute("SELECT * FROM books WHERE id=:id",
+    {"id": book_id}).fetchone()
+    if book is None:
+        return render_template("error.htm", message="No Book was found")        
+    reviews = db.execute("SELECT comment, rate, usr FROM reviews JOIN users ON users.id=user_id WHERE book_id=:book_id LIMIT(4)",
+    {"book_id": book_id}).fetchall()
+    avg_rate=0
+    if len(reviews) !=0:
+        for rev in reviews:
+            avg_rate+=rev.rate
+        avg_rate=avg_rate/len(reviews)
+    user = db.execute("SELECT * FROM users WHERE id=:id",
+    {"id": session["user_id"]}).fetchone()
+    res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                       params={"key": "dwdsoTU7TSH21w2VveT9Q", "isbns": book.isbn})
+    res=res.json()        
+    gdr_rate=res
+    return render_template("comment.htm", user=user, book=book, reviews=reviews, avg_rate=avg_rate, gdr_rate=gdr_rate)
 
 
 @app.route("/search", methods=["GET"])
 def search():
     book_title = request.args.get("searchtxt")
+
     if book_title == None:
         return render_template("error.htm", message="An internal error has ocurred")
     books=db.execute("SELECT * FROM books WHERE LOWER(title)=LOWER(:book_title)",
@@ -94,3 +114,7 @@ def search():
 def logout():
     session["user_id"]=0;
     return render_template("index.htm")
+
+@app.route("/commented")
+def commented():
+    return "Not implemented yet"
--- util.py
+++ util.py
@@ -1,6 +1,13 @@
 import hashlib
+import requests
 
 def encrypt(ps):
     pswd=hashlib.sha256()
     pswd.update(ps.encode('utf-8'))
     return pswd.hexdigest()
+
+
+res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                   params={"key": "dwdsoTU7TSH21w2VveT9Q", "isbns": "042528011X"})
+print(res.json())
+print(res.get("books"))
