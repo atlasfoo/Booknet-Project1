df933d3d529b674388e6b4830c388f10f1c08295
TheFoo @WIN <jf_mejiar547@live.com>
2018-09-23 20:55:24 -0600

    finished comments, pend commented

--- util.py
+++ util.py
@@ -7,7 +7,5 @@ def encrypt(ps):
     return pswd.hexdigest()
 
 
-res = requests.get("https://www.goodreads.com/book/review_counts.json",
-                   params={"key": "dwdsoTU7TSH21w2VveT9Q", "isbns": "042528011X"})
-print(res.json())
-print(res.json()["books"][0]["average_rating"])
+for i in range(5-1):
+    print(i)
