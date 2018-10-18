82bc319a59653262a2f4e0b0ec62acb77869c293
Jerry@Win <jf_mejiar547@live.com>
2018-10-17 22:47:51 -0600

    finished, refined session issues, im ready to submit50

--- util.py
+++ util.py
@@ -1,11 +1,6 @@
 import hashlib
-import requests
 
 def encrypt(ps):
     pswd=hashlib.sha256()
     pswd.update(ps.encode('utf-8'))
     return pswd.hexdigest()
-
-
-for i in range(5-1):
-    print(i)
